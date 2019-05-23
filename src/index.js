7940f59af2461f33b79d9c38a2989c6926729327
Leonard Ginters <leo@l2g.cc>
2019-05-23 20:42:24 +0200

    Better error handling & now compatible with the new Twitter

--- src/index.js
+++ src/index.js
@@ -5,51 +5,48 @@ module.exports = {
     async setup() {
         browser = await puppeteer.launch();
         page = await browser.newPage();
-        page.setUserAgent("Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2049.0 Safari/537.36");
+        page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36");
         await page.setViewport({ width: 1920, height: 1080 });
     },
-    async login(username, password, verificationMethod) {
+    async login(username_or_email, password) {
         await this.setup();
         await page.goto("https://twitter.com/login", { waitUntil: "networkidle2" });
+        await page.evaluate(() => {
+            document.cookie = "rweb_optin=top_no_out; path=/"
+        });
 
         await page.evaluate((username, password) => {
             document.getElementsByClassName("email-input")[1].value = username;
             document.getElementsByClassName("js-password-field")[0].value = password;
 
-            setTimeout(() => {
-                document.querySelectorAll("button.submit")[0].click();
-            }, 500);
-        }, username, password);
+            document.querySelectorAll("button.submit")[0].click();
+        }, username_or_email, password);
 
-        setTimeout(async () => {
-            await page.evaluate((verificationMethod) => {
-                if (document.querySelectorAll("#challenge_response").length > 0) {
-                    document.querySelectorAll("#challenge_response")[0].value = verificationMethod;
+        await page.waitForNavigation();
 
-                    setTimeout(() => {
-                        document.querySelectorAll("#email_challenge_submit")[0].click();
-                    }, 500);
-                }
-            }, verificationMethod);
+        if (page.url().includes("error")) {
+            console.log("Couldn't login.");
+            console.log(`Error: ${page.url().split(".com/")[1]}`);
+            process.exit();
+        }
 
-            if (page.url().includes("error")) {
-                console.log("Couldn't login.");
-                return;
-            }
+        if (page.url().includes("username_disabled=true")) {
+            console.log("Login with username got blocked, please enter your email instead of the username.");
+            process.exit();
+        }
 
-            console.log("Logged in");
-        }, 5000);
+        console.log("Logged in");
     },
     async getTrends() {
-        await page.goto("https://twitter.com/", { waitUntil: "networkidle2" });
+        await page.goto("https://twitter.com/explore", { waitUntil: "networkidle2" });
+
+        await page.screenshot({ path: "evidence.png" });
 
         const trends = await page.evaluate(() => {
             let trends = [];
 
-            for (let i = 0; i < document.querySelectorAll(".trend-name").length; i++) {
-                if (!document.querySelectorAll(".trend-name")[i].parentElement.classList.toString().includes("promoted")) {
-                    trends.push(document.querySelectorAll(".trend-name")[i].innerText);
-                }
+            for (let i = 2; i < 7; i++) {
+                trends.push(document.querySelectorAll(`[style="padding-bottom: 0px;"] > div > div`)[i].querySelectorAll(`[dir="auto"]`)[4].innerText);
             }
 
             return trends;
@@ -60,52 +57,15 @@ module.exports = {
     },
     async tweet(text) {
         if (text.length < 265) {
-            await page.goto("https://twitter.com/", { waitUntil: "networkidle2" });
+            await page.goto("https://twitter.com/compose/tweet", { waitUntil: "networkidle2" });
 
-            await page.evaluate(function(text) {
-                document.querySelectorAll("#global-new-tweet-button")[0].click();
+            await page.type(".public-DraftStyleDefault-block.public-DraftStyleDefault-ltr", text, { delay: 20 });
+            await page.evaluate(() => {
+                document.querySelectorAll(`[role="button"][tabindex="0"] > div > span > span`)[0].click();
+            });
 
-                setTimeout(() => {
-                    document.querySelectorAll("div[name='tweet'] > div")[1].innerText = text;
-
-                    setTimeout(() => {
-                        document.querySelectorAll(".SendTweetsButton")[0].click();
-                    }, 500);
-                }, 500);
-            }, text);
-            console.log(`Tweeted '${text}'`);
-            return Buffer.from(text).toString("base64");
+            console.log(`Tweeted "${text}"`);
         }
-    },
-    async deleteTweet(identifier, username) {
-        identifier = Buffer.from(identifier, "base64").toString("ascii");
-
-        await page.goto(`https://twitter.com/${username}/with_replies`, { waitUntil: "networkidle2" });
-
-        await page.evaluate((identifier) => {
-            let id = null;
-            for (let i = 0; i < document.querySelectorAll(".TweetTextSize").length; i++) {
-                if (document.querySelectorAll(".TweetTextSize")[i].innerText === identifier) {
-                    id = i;
-                }
-            }
-
-            if (id != null) {
-                document.querySelectorAll(".TweetTextSize")[id].parentElement.parentElement.firstElementChild.lastElementChild.firstElementChild.firstElementChild.click();
-
-                setTimeout(() => {
-                    document.querySelectorAll(".TweetTextSize")[id].parentElement.parentElement.firstElementChild.lastElementChild.firstElementChild.lastElementChild.children[2].children[5].click();
-
-                    setTimeout(() => {
-                        document.querySelectorAll(".modal-content .delete-action")[0].click();
-                    }, 2000);
-                }, 1000);
-            }else {
-                console.log(`Coudn't find tweet '${identifier}'`);
-            }
-        }, identifier);
-
-        console.log(`Deleted tweet '${identifier}'`);
     },
     async query(advancedSearchURL) {
         await page.goto(advancedSearchURL, { waitUntil: "networkidle2" });
@@ -113,9 +73,9 @@ module.exports = {
         return await page.evaluate(() => {
             let tweets = [];
 
-            for (let i = 0; i < document.querySelectorAll("#timeline .stream-item-header .account-group > span.username").length; i++) {
-                let username = document.querySelectorAll("#timeline .stream-item-header .account-group > span.username")[i].innerText;
-                let text = document.querySelectorAll("#timeline .stream-item-header .account-group > span.username")[i].parentElement.parentElement.parentElement.getElementsByClassName("js-tweet-text-container")[0].innerText;
+            for (let i = 0; i < document.querySelectorAll("article").length; i++) {
+                let username = document.querySelectorAll("article")[i].querySelector("div").children[1].querySelector("div").innerText.split("@")[1].split("\n")[0];
+                let text = document.querySelectorAll("article")[i].querySelector("div").children[1].querySelector("div").lastElementChild.innerText;
 
                 tweets.push({username: username, text: text})
             }
@@ -126,23 +86,8 @@ module.exports = {
     async follow(username) {
         await page.goto(`https://twitter.com/${username}`, { waitUntil: "networkidle2" });
 
-        await page.evaluate(() => {
-            document.querySelector(".follow-text").click();
-        });
+        await page.click(`[style="min-width: 79px;"] > [role="button"]`);
 
         console.log(`Followed '${username}'`);
-    },
-    async getUserFollowers(username) {
-        await page.goto(`https://twitter.com/${username}/followers`, { waitUntil: "networkidle2" });
-
-        return await page.evaluate(() => {
-            let usernames = [];
-
-            for (let i = 0; i < document.querySelectorAll(".ProfileCard-screennameLink .username").length; i++) {
-                usernames.push(document.querySelectorAll(".ProfileCard-screennameLink .username")[i].innerText);
-            }
-
-            return usernames;
-        });
     }
 };

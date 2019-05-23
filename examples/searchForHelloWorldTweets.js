4c24ae9f65fdbeddeb4669d7eff72270c6fb6ddd
Leonard Ginters <leo@l2g.cc>
2019-05-23 20:47:29 +0200

    Update to newest version

--- examples/searchForHelloWorldTweets.js
+++ examples/searchForHelloWorldTweets.js
@@ -1,13 +1,10 @@
 const twitall = require('twitall');
 
 (async () => {
-    const username = "imausername";
-    const password = "imapassword";
+    const username = "testusername";
+    const password = "testpassword";
 
-    await twitall.login(username, password, "1900 614 321");
-    setTimeout(async () => {
-        const tweets = await twitall.query("https://twitter.com/search?l=&q=%22hello%20world%22&src=typd");
-
-        console.log(tweets); // [{ username: '@cra1zer', text: 'hello world!' }, { username: '@Carissa_lovee', text: 'Hello world ' },...]
-    }, 10000);
+    await twitall.login(username, password);
+    const tweets = await twitall.query("https://twitter.com/search?l=&q=%22hello%20world%22&src=typd");
+    console.log(tweets); // [{ username: '@cra1zer', text: 'hello world!' }, { username: '@Carissa_lovee', text: 'Hello world ' },...]
 })();

3d0e31bddc099a17483ceea53ef6ef81f55da9e4
Leonard Ginters <leo@l2g.cc>
2019-05-23 20:46:20 +0200

    Update to newest version

--- examples/getTrends.js
+++ examples/getTrends.js
@@ -1,12 +1,10 @@
 const twitall = require('twitall');
 
 (async () => {
-    const username = "imausername";
-    const password = "imapassword";
+    const username = "testusername";
+    const password = "testpassword";
 
-    await twitall.login(username, password, "1900 614 321");
-    setTimeout(async () => {
-        const trends = await twitall.getTrends();
-        console.log(trends); // ['Joker', 'Joaquin Phoenix', '#HukuktanKorkma',...]
-    }, 10000);
+    await twitall.login(username, password);
+    const trends = await twitall.getTrends();
+    console.log(trends); // ['Joker', 'Joaquin Phoenix', '#HukuktanKorkma',...]
 })();
